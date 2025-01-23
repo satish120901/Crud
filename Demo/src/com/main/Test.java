8f8ad4df4eb41c3a990ecd040ebd1c125db39e57
satis <satis@SatishAgrawal>
2025-01-23 16:19:04 +0530

    Added

---
+++ Demo/src/com/main/Test.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class Test {
+	 int a=10;
+	 int b=5;
+	public void add() {
+		
+		System.out.println(a+b);
+	}
+public static void main(String[] args) {
+	Test t=new Test();
+	t.add();
+}
+}
